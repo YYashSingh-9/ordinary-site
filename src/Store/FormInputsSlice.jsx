3aa838f9ffcd848645ea19564d0d2ba7e75cf830
YYashSingh-9 <yyashsngh@gmail.com>
2023-10-05 09:07:07 -0700

    redux updates

--- src/Store/FormInputsSlice.jsx
+++ src/Store/FormInputsSlice.jsx
@@ -6,11 +6,30 @@ const initialStateTwo = {
   passwordInput: "",
   passwordConfirmInput: "",
   date_of_birth_Input: "",
-  mobileNumberInput: "",
+  mobileNumberInput: 0,
 };
 
 const FormSlice = createSlice({
   name: "SliceTwo",
   initialState: initialStateTwo,
-  reducers: {},
+  reducers: {
+    inputFieldValHandler(state, action) {
+      const inputObj = action.payload;
+      inputObj.field_name === "name" ? (state.nameInput = inputObj.input) : "";
+      inputObj.field_name === "email" ? (state.nameInput = inputObj.input) : "";
+      inputObj.field_name === "password"
+        ? (state.nameInput = inputObj.input)
+        : "";
+      inputObj.field_name === "passwordconfirm"
+        ? (state.nameInput = inputObj.input)
+        : "";
+      inputObj.field_name === "dob" ? (state.nameInput = inputObj.input) : "";
+      inputObj.field_name === "mobile"
+        ? (state.nameInput = inputObj.input)
+        : "";
+    },
+  },
 });
+
+export const formAction = FormSlice.actions;
+export default FormSlice;
--- src/Store/MainStore.jsx
+++ src/Store/MainStore.jsx
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import StoreSlice from "./StoreSlice";
-
-const MainStore = configureStore({ reducer: { sliceOne: StoreSlice.reducer } });
+import FormSlice from "./FormInputsSlice";
+const MainStore = configureStore({
+  reducer: { sliceOne: StoreSlice.reducer, sliceTwo: FormSlice.reducer },
+});
 export default MainStore;
