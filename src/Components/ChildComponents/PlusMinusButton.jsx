aab016bc3ee75426067cf9aec8beb1236607a6f9
YYashSingh-9 <yyashsngh@gmail.com>
2023-10-09 07:21:42 -0700

    minors

--- src/Components/ChildComponents/PlusMinusButton.jsx
+++ src/Components/ChildComponents/PlusMinusButton.jsx
@@ -1,16 +1,12 @@
 import classes from "./PlusMinusButton.module.css";
 import { useState } from "react";
-import { actions } from "../../Store/StoreSlice";
-import { useDispatch } from "react-redux";
 
 // THIS COMPONENT CAN BE SEEN IN PRODUCT SHOWN IN CART PAGE WHEN ADDED TO CART(BAG)
 // MADE THIS WAY BECAUSE IT WAS USED TWICE THRICE SO -> D.R.Y
 const PlusMinusButton = (props) => {
   const [quantityState, setQuantity] = useState(props.elems.quantity);
-  const dispatch = useDispatch();
 
   const incrementHandler = () => {
-    // dispatch(actions.AddItemToCart(props.elems));
     props.addItemFunction();
     setQuantity((state) => {
       return state + 1;
