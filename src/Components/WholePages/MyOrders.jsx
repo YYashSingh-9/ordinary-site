27a4020830ab2cfc0af7205c43a6c3cdf76d98fe
YYashSingh-9 <yyashsngh@gmail.com>
2023-10-10 09:09:32 -0700

    error

--- src/Components/WholePages/MyOrders.jsx
+++ src/Components/WholePages/MyOrders.jsx
@@ -72,6 +72,7 @@ const MyOrders = () => {
       return getMyOrders(cookie);
     },
   });
+  console.log(data);
   const dispatch = useDispatch();
   //THIS IS DONE TO REMOVE SUB MENU BECAUSE USER CLICKS THE SCREEN TO REMOVE POPUPS WHICH IS WE TARGET THIS DIV FOR THIS WORK
   const submenuRemover = () => {
