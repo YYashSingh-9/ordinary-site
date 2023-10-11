5a7d1652b6cf02a0eb24bd4e9aba958a543fc7be
YYashSingh-9 <yyashsngh@gmail.com>
2023-10-11 03:58:03 -0700

    route finished

--- backend/routes/FavRouter.js
+++ backend/routes/FavRouter.js
@@ -1,2 +1,11 @@
 const express = require("express");
+const FavController = require("../controllers/FavProductController");
+const AuthController = require("../controllers/AuthController");
 const FavsRouter = express.Router();
+
+FavsRouter.use(AuthController.protect);
+FavsRouter.route("/my-favs")
+  .get(FavController.getYourFavs)
+  .post(FavController.postFavProduct);
+
+module.exports = FavsRouter;
